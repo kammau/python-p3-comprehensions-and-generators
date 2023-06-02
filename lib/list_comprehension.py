f7da2eae0f85e30b2ec2776872087ee6d3dcff81
Kamryn Smith <programersign@gmail.com>
2023-06-02 14:56:51 -0700

    assignment complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    num_comp = [i for i in num_list if i % 2 == 0]
+    return num_comp
 
 def make_exclamation(sentence_list):
-    pass
+    new_list = [f"{i}!" for i in sentence_list]
+    return new_list
